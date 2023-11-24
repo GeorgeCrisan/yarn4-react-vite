173e6d82174fc7fcf30eb7ea19c1fe07fe1be0be
George Crisan <georgerdp@gmail.com>
2023-11-24 20:15:39 +0000

    [chore] Init yarn 4 + vite + react

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# yarn3-example
+# yarn 4 + React + Vite 
+<a href="https://hypejs.com" targer="_blank"> By hypejs.com </a>
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import viteLogo from '/hypejs.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://hypejs.com" target="_blank">
+          <img src={viteLogo} className="logo" alt="hypejs logo" />
+        </a>
+      </div>
+      <h1>yarn 4 + Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
